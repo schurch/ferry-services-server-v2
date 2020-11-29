a9c92a089098dc98c4df9cf914b14a91ebcee71d
Stefan Church <stefan.church@gmail.com>
2020-11-29 16:30:57 +1300

    Add new homepage with ferry service statuses

---
+++ public/precache-manifest.bdcfe42a6f1f5732518da96774f16c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ac4179ac5f611108988ba0510bc144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e087f3e47899f291f389",
+    "url": "/static/css/main.56c2eec8.chunk.css"
+  },
+  {
+    "revision": "5f3d59071a5c4f27db55",
+    "url": "/static/js/2.d4bda92f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4bda92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e087f3e47899f291f389",
+    "url": "/static/js/main.9cbb3dee.chunk.js"
+  },
+  {
+    "revision": "5355959976867d9ff6d7",
+    "url": "/static/js/runtime-main.8830f1d3.js"
+  }
+]);
