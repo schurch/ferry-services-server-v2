608cd7dd26542884f31d98b9d1c63c7f55a86258
Stefan Church <stefan.church@gmail.com>
2021-11-02 20:18:55 +1300

    Add northlink migration

---
+++ migrations/000009_add-northlink-service.down.sql
@@ -0,0 +1,15 @@
+ALTER TABLE services DROP COLUMN IF EXISTS organisation;
+
+DELETE FROM service_locations WHERE service_id = 1000 AND location_id = 59;
+DELETE FROM service_locations WHERE service_id = 1000 AND location_id = 60;
+DELETE FROM service_locations WHERE service_id = 1000 AND location_id = 61;
+DELETE FROM service_locations WHERE service_id = 1000 AND location_id = 62;
+DELETE FROM service_locations WHERE service_id = 1000 AND location_id = 63;
+
+DELETE FROM locations WHERE location_id = 59;
+DELETE FROM locations WHERE location_id = 60;
+DELETE FROM locations WHERE location_id = 61;
+DELETE FROM locations WHERE location_id = 62;
+DELETE FROM locations WHERE location_id = 63;
+
+DELETE FROM services WHERE service_id = 1000;
---
+++ migrations/000009_add-northlink-service.up.sql
@@ -0,0 +1,16 @@
+ALTER TABLE services ADD COLUMN organisation TEXT NOT NULL DEFAULT 'CalMac';
+ALTER TABLE services ALTER COLUMN organisation DROP DEFAULT;
+
+INSERT INTO services (service_id, sort_order, area, route, organisation, status, updated) VALUES (1000, '24', 'ORKNEY & SHETLAND', 'Scrabster - Stromness / Aberdeen - Kirkwall - Lerwick', 'NorthLink', -99, now());
+
+INSERT INTO locations (location_id, name, latitude, longitude) VALUES (59, 'Scrabster', 58.61252430922174, -3.545534457670658);
+INSERT INTO locations (location_id, name, latitude, longitude) VALUES (60, 'Stromness', 58.96421632953152, -3.2954037645564296);
+INSERT INTO locations (location_id, name, latitude, longitude) VALUES (61, 'Aberdeen', 57.145172030019346, -2.091230925036305);
+INSERT INTO locations (location_id, name, latitude, longitude) VALUES (62, 'Kirkwall', 58.99989823953207, -2.9742511887813246);
+INSERT INTO locations (location_id, name, latitude, longitude) VALUES (63, 'Lerwick', 60.16270380403575, -1.1589239389948884);
+
+INSERT INTO service_locations (service_id, location_id) VALUES (1000, 59);
+INSERT INTO service_locations (service_id, location_id) VALUES (1000, 60);
+INSERT INTO service_locations (service_id, location_id) VALUES (1000, 61);
+INSERT INTO service_locations (service_id, location_id) VALUES (1000, 62);
+INSERT INTO service_locations (service_id, location_id) VALUES (1000, 63);
