93561e7c9e982bf5f6fe3ccda44a187f799be49d
Stefan Church <stefan.church@gmail.com>
2022-04-24 14:01:25 +1200

    Add down sql for coordinates

---
+++ migrations/000013_use-geometry-for-coordinates.down.sql
@@ -0,0 +1,15 @@
+ALTER TABLE locations ADD COLUMN latitude NUMERIC NULL;
+ALTER TABLE locations ADD COLUMN longitude NUMERIC NULL;
+UPDATE locations SET latitude = ST_X(coordinate);
+UPDATE locations SET longitude = ST_Y(coordinate);
+ALTER TABLE locations ALTER COLUMN latitude SET NOT NULL;
+ALTER TABLE locations ALTER COLUMN longitude SET NOT NULL;
+ALTER TABLE locations DROP COLUMN coordinate;
+
+ALTER TABLE vessels ADD COLUMN latitude NUMERIC NULL;
+ALTER TABLE vessels ADD COLUMN longitude NUMERIC NULL;
+UPDATE vessels SET latitude = ST_X(coordinate);
+UPDATE vessels SET longitude = ST_Y(coordinate);
+ALTER TABLE vessels ALTER COLUMN latitude SET NOT NULL;
+ALTER TABLE vessels ALTER COLUMN longitude SET NOT NULL;
+ALTER TABLE vessels DROP COLUMN coordinate;
