227d97731d1de5eeb48851eecbaa152859740792
Stefan Church <stefan.church@gmail.com>
2023-07-07 21:24:07 +1200

    Fix capitalisation of  McInroy's

--- migrations/000014_add-organisation.up.sql
+++ migrations/000014_add-organisation.up.sql
@@ -18,7 +18,7 @@ ALTER TABLE services ALTER COLUMN organisation_id DROP DEFAULT;
 ALTER TABLE vessels ADD COLUMN organisation_id INT NOT NULL DEFAULT 1 REFERENCES organisations (organisation_id);
 UPDATE vessels SET organisation_id = 2 WHERE mmsi IN (235449000, 235450000, 235448000);
 
-INSERT INTO services (service_id, area, route, organisation_id, status, updated) VALUES (2000, 'COWAL & DUNOON (Western Ferries)', 'Mcinroy''s Point (Gourock) - Hunters Quay (Dunoon)', 3, -99, now());
+INSERT INTO services (service_id, area, route, organisation_id, status, updated) VALUES (2000, 'COWAL & DUNOON (Western Ferries)', 'McInroy''s Point (Gourock) - Hunters Quay (Dunoon)', 3, -99, now());
 
 INSERT INTO locations (location_id, name, coordinate) VALUES (64, 'McInroy''s Point', ST_SetSRID(ST_Point(55.95160698614026, -4.85339972935733), 4326));
 INSERT INTO locations (location_id, name, coordinate) VALUES (65, 'Hunters Quay', ST_SetSRID(ST_Point(55.970673603254305, -4.909260840891939), 4326));
