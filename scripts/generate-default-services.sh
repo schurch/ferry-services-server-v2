068713182f9898d0f64a6138fe4ce44a81d97bfe
Stefan Church <stefan.church@gmail.com>
2023-07-29 17:49:54 +1200

    Don’t include disruption_reason in default json; update services.json

--- scripts/generate-default-services.sh
+++ scripts/generate-default-services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://scottishferryapp.com/api/services/ | jq '. | .[].status |= -99 | .[].additional_info |= null | .[].last_updated_date |= null | .[].updated |= null | .[].vessels |= [] | .[].locations[].weather |= null'  > services.json
+curl https://scottishferryapp.com/api/services/ | jq '. | .[].status |= -99 | .[].additional_info |= null | .[].disruption_reason |= null | .[].last_updated_date |= null | .[].updated |= null | .[].vessels |= [] | .[].locations[].weather |= null'  > services.json
